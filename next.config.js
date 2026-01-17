e6c3d79c4c5c8c53b47cf0083f6c1a10878b513f
Azhwin2005 <ashwinarun05@gmail.com>
2026-01-17 10:16:19 +0530

    fix(build): add supabase keys to next.config.js for vercel deployment

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const withPWA = require("next-pwa")({
 
 const nextConfig = {
     // Add other config here
+    env: {
+        NEXT_PUBLIC_SUPABASE_URL: "https://bsaicgzweugkzkyernvx.supabase.co",
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzYWljZ3p3ZXVna3preWVybnZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5MzcyMzgsImV4cCI6MjA4MjUxMzIzOH0.PU3WgKAxu0nZ5Gduj6V9t-oYMUddm711zONg3yTUlAM",
+    }
 };
 
 module.exports = withPWA(nextConfig);
